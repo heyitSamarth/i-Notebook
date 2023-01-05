8ff485e4ec83a516f2ded814c85b078d6648c51d
Samarth Dubey <samarthdubey49@gmail.com>
2023-01-05 18:51:11 +0530

    day 2 (deploy)

--- Backend/db.js
+++ Backend/db.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose')
-const mongoURI="mongodb://localhost:27017/inotebook"
-// const mongoURI="mongodb+srv://Heyitsamarth:Password1@inotebook.5garq7c.mongodb.net/inotebook"
+// const mongoURI="mongodb://localhost:27017/inotebook"
+const mongoURI="mongodb+srv://Heyitsamarth:Password1@inotebook.5garq7c.mongodb.net/?retryWrites=true&w=majority"
 
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>
-    {
-        console.log("connected to mongo succesfull")
-    })
+    // mongoose.connect(mongoURI,()=>
+    // {
+    //     console.log("connected to mongo succesfull")
+    // })
+    mongoose.connect(
+        mongoURI,
+        // options,
+        (err) => {
+         if(err) console.log(err) 
+         else console.log("connected to mongo succesfull");
+        }
+      );
 }
 
 module.exports=connectToMongo;
